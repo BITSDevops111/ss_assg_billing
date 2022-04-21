19a8705c514d42c48d2abcb09d079aff492c3f4d
himanshu chaurishiya <81251470+BITSDevops111@users.noreply.github.com>
2022-04-21 06:05:08 +0530

    Microservice API -2020MT93636
    Contains dockerfile

---
+++ models.py
@@ -0,0 +1,24 @@
+from db import db, metadata, sqlalchemy
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("first_name", sqlalchemy.String),
+    sqlalchemy.Column("last_name", sqlalchemy.String),
+    sqlalchemy.Column("amount", sqlalchemy.Integer),
+)
+
+
+class User:
+    @classmethod
+    async def get(cls, id):
+        query = users.select().where(users.c.id == id)
+        user = await db.fetch_one(query)
+        return user
+
+    @classmethod
+    async def create(cls, **user):
+        query = users.insert().values(**user)
+        user_id = await db.execute(query)
+        return user_id
